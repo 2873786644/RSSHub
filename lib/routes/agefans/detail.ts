f047d6f30e2d96bff1694a51e1f96fb691237beb
2873786644 <97467706+2873786644@users.noreply.github.com>
2025-03-26 09:17:32 +0800

    Update detail.ts
    更该网站域名

--- lib/routes/agefans/detail.ts
+++ lib/routes/agefans/detail.ts
@@ -18,7 +18,7 @@ export const route: Route = {
     },
     radar: [
         {
-            source: ['agemys.org/detail/:id'],
+            source: ['agedm.org/detail/:id'],
         },
     ],
     name: '番剧详情',
